fd517845ee85a17941971b85bb9266dddb06923b
TendaniMamadi <mamadi.tendani@gmail.com>
2023-05-25 16:05:53 +0200

    all functions works and tests passed

--- taxi-queue.dom.js
+++ taxi-queue.dom.js
@@ -2,10 +2,49 @@
 
 
 // DOM element references
+var passengerCount = document.querySelector(".passenger_queue_count");
+var passengerJoinBtn = document.querySelector(".join_queue");
+var passengerLeaveBtn = document.querySelector(".leave_queue");
+
+var taxiCount = document.querySelector(".taxi_queue_count");
+var taxiJoinQue = document.querySelector(".join_taxi_queue");
+var taxiDeparture = document.querySelector(".depart");
+
+
 
 // create Factory Function instance
 
 const taxiQueue = TaxiQueue();
-
 // DOM events
+
+passengerJoinBtn.addEventListener('click',function() {
+ 
+    taxiQueue.joinQueue();
+ 
+    passengerCount.innerHTML = taxiQueue.queueLength(); 
+});
+
+passengerLeaveBtn.addEventListener('click',function() {
+ 
+    taxiQueue.leaveQueue();
+ 
+    passengerCount.innerHTML = taxiQueue.queueLength(); 
+});
+
+taxiJoinQue.addEventListener('click',function() {
+ 
+    taxiQueue.joinTaxiQueue();
+ 
+    taxiCount.innerHTML = taxiQueue.taxiQueueLength(); 
+});
+
+taxiDeparture.addEventListener('click',function() {
+ 
+    taxiQueue.taxiDepart();
+    taxiQueue.queueLength(); 
+ 
+    taxiCount.innerHTML = taxiQueue.taxiQueueLength(); 
+    passengerCount.innerHTML = taxiQueue.queueLength(); 
+});
+
 
--- taxi-queue.js
+++ taxi-queue.js
@@ -1,28 +1,54 @@
 function TaxiQueue() {
-
+	//create a variable for passengers joining queue
+	//create a variable for taxis joining queue
+	var Passengers = 0;
+	var taxis = 0;
+	
 
 	function joinQueue() {
-
+		//add passengers to the queue
+		Passengers++;
 	}
 
 	function leaveQueue() {
+		//minus passengers to the queue
+		if(Passengers > 0){
+			Passengers--;
+		}
 
 	}
 
 	function joinTaxiQueue() {
+		//add taxis to the queue when passengers are more than one.
+		
+		if(Passengers >= 0){
+			taxis++;
+		}
 
 	}
 
 	function queueLength() {
+		//return the counter for both joinQueue and leaveQueue
+		if(Passengers >= 0){
+
+			return Passengers;
+		}
 
 	}
 
 	function taxiQueueLength() {
+		//return the number of taxis in a queue
+		return taxis;
 
 	}
 
 	function taxiDepart(){
+		//if passengers are more than 12 taxi should depart(minus 12 passengers in queue and minus 1 taxi in taxi queue)
+		if(Passengers >= 12 && taxis != 0){
 
+			Passengers-= 12
+			taxis--
+		}
 	}
 
 	return {
